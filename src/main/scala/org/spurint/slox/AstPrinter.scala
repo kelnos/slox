270a277e211d4076398efc185716b6d51ab25080
Brian J. Tarricone <brian@tarricone.org>
2017-10-31 00:55:44 -0700

    add Expr representation and AstPrinter for it

---
+++ src/main/scala/org/spurint/slox/AstPrinter.scala
@@ -0,0 +1,29 @@
+package org.spurint.slox
+
+import org.spurint.slox.Expr.{Binary, Grouping, Literal, Unary}
+
+trait AstPrinter[T] {
+  def apply(printable: T): String
+}
+
+object AstPrinter {
+  def apply[T](printable: T)(implicit printer: AstPrinter[T]): String = printer(printable)
+}
+
+object ExprAstPrinters {
+  private def parenthesize[T <: Expr](name: String, exprs: T*)(implicit printable: AstPrinter[T]): String = {
+    "(" + name + exprs.map(expr => " " + AstPrinter(expr)).mkString + ")"
+  }
+
+  implicit val unaryPrinter: AstPrinter[Unary] = (expr: Unary) => parenthesize(expr.operator.lexeme, expr.right)
+  implicit val binaryPrinter: AstPrinter[Binary] = (expr: Binary) => parenthesize(expr.operator.lexeme, expr.left, expr.right)
+  implicit val groupingPrinter: AstPrinter[Grouping] = (expr: Grouping) => parenthesize("group", expr.expression)
+  implicit val literalPrinter: AstPrinter[Literal] = (expr: Literal) => expr.value.map(v => v.toString).getOrElse("nil")
+
+  implicit val genericExprPrinter: AstPrinter[Expr] = {
+    case u: Unary => unaryPrinter(u)
+    case b: Binary => binaryPrinter(b)
+    case g: Grouping => groupingPrinter(g)
+    case l: Literal => literalPrinter(l)
+  }
+}
---
+++ src/main/scala/org/spurint/slox/Expr.scala
@@ -0,0 +1,10 @@
+package org.spurint.slox
+
+sealed trait Expr
+
+object Expr {
+  case class Binary(left: Expr, operator: Token, right: Expr) extends Expr
+  case class Grouping(expression: Expr) extends Expr
+  case class Literal(value: Option[Any]) extends Expr
+  case class Unary(operator: Token, right: Expr) extends Expr
+}
