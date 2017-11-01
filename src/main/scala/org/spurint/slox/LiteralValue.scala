691289816939979999cc84ce95041599968912d7
Brian J. Tarricone <brian@tarricone.org>
2017-10-31 17:42:00 -0700

    better printing of integers

--- src/main/scala/org/spurint/slox/LiteralValue.scala
+++ src/main/scala/org/spurint/slox/LiteralValue.scala
@@ -6,9 +6,17 @@ sealed trait LiteralValue[T] {
 
 object LiteralValue {
   case object NilValue extends LiteralValue[Any] { override val toString: String = "nil"; val value: Any = null }
-  case class NumberValue(value: Double) extends LiteralValue[Double] { override val toString: String = value.toString}
+  case class NumberValue(value: Double) extends LiteralValue[Double] { override val toString: String = fmtNumber(value) }
   case class StringValue(value: String) extends LiteralValue[String] { override val toString: String = value }
   case class BooleanValue(value: Boolean) extends LiteralValue[Boolean] { override val toString: String = value.toString }
   case class IdentifierValue(value: String) extends LiteralValue[String] { override val toString: String = value }
   case class CommentValue(value: String) extends LiteralValue[String] { override val toString: String = value }
+
+  private def fmtNumber(number: Double): String = {
+    if (number > Long.MaxValue || number < Long.MinValue || number.toLong != number) {
+      number.toString
+    } else {
+      number.toLong.toString
+    }
+  }
 }
